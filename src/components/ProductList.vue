fc95b1d16cdf0ed74b5b546066fcecf47f4c2788
alexgah <alexgah@yandex.ru>
2020-11-21 18:21:53 +0300

    first commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,14 @@
+<template>
+  <ProductItem :products="products"></ProductItem>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  props: ['products'],
+  components: {
+    ProductItem,
+  },
+};
+</script>
---
+++ src/data/products.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-1.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-2.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-3.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-4.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-5.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-6.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-7.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-8.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-9.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-10.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-11.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-12.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-13.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-14.png',
+  },
+  {
+    title: 'Тест',
+    price: 300,
+    image: '/img/guitars/guitar-15.png',
+  },
+];
